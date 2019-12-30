c2054c7e5ecca6999a300b0da1d4a94c61fd4205
TheMindset <mulezig2014@outlook.fr>
2019-12-30 03:28:37 +0100

    Add jest.config.js & install dotenv

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "node",
+  maxConcurrency: 1,
+  globalSetup: "./tests/helpers/testGlobalSetup.js",
+  globalTeardown: "./tests/helpers/testGlobalTeardown.js"
+
+}
