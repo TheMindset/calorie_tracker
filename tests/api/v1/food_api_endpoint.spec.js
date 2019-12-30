e260b2c9f6ec9d20a36ce2bcfab3cf60b2413aa2
TheMindset <mulezig2014@outlook.fr>
2019-12-30 07:28:22 +0100

    Add food_all spec endpoint

---
+++ tests/api/v1/food_api_endpoint.spec.js
@@ -0,0 +1,45 @@
+const request = require('superset')
+
+const app = require('../../../app')
+const Food = require('../../../models').Food
+const cleanup = require('../../helpers')
+
+describe('Food api endpoint', () => {
+  test('should returns all foods', () => {
+    return Food.bulkCreate([
+      {
+          name: "Banana",
+          calories: 150,
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+      {
+          name: "Apple",
+          calories: 100,
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+      {
+          name: "Kiwi",
+          calories: 700,
+          createdAt: new Date(),
+          updatedAt: new Date()
+      }
+    ]).then(() => {
+      return request(app)
+      .get('/api/v1/foods')
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+
+        expect(response.body.length).toBe(3)
+        expect(Object.keys(response.body[0])).toContain('id')
+        expect(Object.keys(response.body[0])).toContain('name')
+        expect(Object.keys(response.body[0])).toContain('calories')
+
+        expect(Object.keys(response.body[0])).toContain('createdAt')
+        expect(Object.keys(response.body[0])).toContain('updatedAt')
+      })
+  
+    })
+  })
+})
