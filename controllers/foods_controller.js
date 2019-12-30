5ea04bfbc685c495dd7924c152278562018f5b29
TheMindset <mulezig2014@outlook.fr>
2019-12-30 11:23:13 +0100

    Add show method

--- controllers/foods_controller.js
+++ controllers/foods_controller.js
@@ -1,6 +1,7 @@
 const Food = require('../models').Food
 
 const index = (request, response) => {
+  console.log(request)
   return Food.findAll({
     attributes: { exclude: ['createdAt', 'updatedAt']}
   })
@@ -14,6 +15,34 @@ const index = (request, response) => {
   })
 }
 
+const show = (request, response) => {
+  console.log(request.params)
+
+  return Food.findOne({
+    attributes: { 
+      exclude: ['createdAt', 'updatedAt']
+    },
+    where: {
+      id: request.params.id
+    }
+  })
+  .then(food => {
+    if (food) {
+      response.setHeader('Content-type', 'application/json')
+      response.status(200).send(JSON.stringify(food))
+    } else {
+      response.setHeader('Content-type', 'application/json')
+      response.status(404).send(JSON.stringify({
+        error: "Food not found."
+      }))
+    }
+  })
+  .catch(error =>{
+    response.setHeader('Content-type', 'application/json')
+    response.status(500).send({ error })
+  })
+}
+
 module.exports = {
-  index,
+  index, show
 }
