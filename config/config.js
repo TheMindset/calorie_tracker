158fd64c1670089022fce61f7623bc6f8cba3df4
TheMindset <mulezig2014@outlook.fr>
2019-12-30 06:55:28 +0100

    require dotenv

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": null,
-    "database": "calorie_tracker_development",
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "postgres",
     "operatorsAliases": 0
