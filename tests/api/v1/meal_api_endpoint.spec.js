018a7f795c7ed8ecad8e2e592154060360c5da21
TheMindset <mulezig2014@outlook.fr>
2020-01-02 21:25:02 +0100

    Add  mealFood endpoint spec

--- tests/api/v1/meal_api_endpoint.spec.js
+++ tests/api/v1/meal_api_endpoint.spec.js
@@ -2,6 +2,7 @@ const app = require('../../../app')
 const request = require('supertest')
 
 const Meal = require('../../../models').Meal
+const Food = require('../../../models').Food
 
 const cleanup = require('../../helpers/testCleanupDatabase')
 
@@ -59,6 +60,43 @@ describe('meals api endpoint', () => {
 
         expect(Object.keys(response.body[0].foods[0])).not.toContain('createdAt')
         expect(Object.keys(response.body[0].foods[0])).not.toContain('updatedAt')
+      })
+    })
+  })
+
+  test('should associate food & meal with food_id & meal_id', () => {
+    return Meal.create({
+      name: 'Dinner'
+    })
+    .then(meal => {
+      return Food.create({
+        name: 'Salad',
+        calories: 260
+      })
+      .then(food => {
+        return request(app)
+        .post(`/api/v1/meals/${meal.id}/foods/${food.id}`)
+        .then(response => {
+          expect(response.statusCode).toBe(201)
+
+          expect(Object.keys(response.body)).toContain('message')
+          expect(Object.values(response.body)).toContain(`Successfuly added ${food.name} to ${meal.name}`)
+        })
+      })
+    })
+  })
+
+  test('should send a 404 error when the meal is not found', () => {
+    return Food.create({
+      name: 'Harissa',
+      calories: 120
+    })
+    .then(food => {
+      return request(app)
+      .post(`/api/v1/meals/70/foods/${food.id}`)
+      .then(response => {
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Meal not found')
       })
     })
   })
