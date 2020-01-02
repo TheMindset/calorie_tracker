cbf0ffcd876d7c549c48bdb91d17ce38b7221daa
TheMindset <mulezig2014@outlook.fr>
2020-01-02 16:25:51 +0100

    Add association test

--- tests/models/food.spec.js
+++ tests/models/food.spec.js
@@ -1,5 +1,7 @@
 const app = require('../../app')
 const Food = require('../../models').Food
+const Meal = require('../../models').Meal
+
 
 const cleanup = require('../helpers/testCleanupDatabase')
 
@@ -8,7 +10,7 @@ describe('Food Model', () => {
     cleanup()
   })
 
-  test('#attributes', () => {
+  test('it has attributes', () => {
     return Food.create({
       name: 'Bananas',
       calories: 120,
@@ -18,5 +20,62 @@ describe('Food Model', () => {
       expect(food.dataValues.calories).toBe('120')
     })
   })
+
+  test('it uppercase the first word of name attribute', () => {
+    return Food.create({
+      name: 'kiwi',
+      calories: 700,
+    })
+    .then(food => {
+      expect(food.dataValues.name).toBe('Kiwi')
+    })
+  })
+  
+  it('it cannot a food with a name already in database', () => {
+    return Food.create({
+      name: 'kiwi',
+      calories: 700,
+    })
+    .then(food => {
+      return Food.create({
+        name: 'kiwi',
+        calories: 700,
+      })
+      .catch(error => {
+        expect(error.name).toBe('SequelizeUniqueConstraintError')
+      })
+    })
+  })
+
+  test('it associated with meals', () => {
+    return Food.create({
+      name: 'kiwi',
+      calories: 700,
+    })
+    .then(food => {
+      return Meal.create({
+        name: 'First Meal'
+      })
+      .then(meal => {
+        // https://sequelize.org/master/class/lib/associations/has-many.js~HasMany.html#instance-method-has
+        // check association
+        return food.hasMeal(meal)
+        .then(result => {
+          expect(result).toBe(false)
+          //https://sequelize.org/master/class/lib/associations/has-many.js~HasMany.html#instance-method-add
+          // add association
+          return food.addMeal(meal)
+        })
+        .then(() => {
+          return food.hasMeal(meal)
+        })
+        .then(result => {
+          console.log(result)
+          expect(result).toBe(true)
+        })
+      })
+    })
+  })
+  
   
 })
