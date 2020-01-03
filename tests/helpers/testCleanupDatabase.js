dc17758e4e1739e497411d0deb2dee52b7e43344
TheMindset <mulezig2014@outlook.fr>
2020-01-03 14:10:19 +0100

    Add cleanup Meal table before Each test

--- tests/helpers/testCleanupDatabase.js
+++ tests/helpers/testCleanupDatabase.js
@@ -1,5 +1,7 @@
 const Food = require('../../models').Food
+const Meal = require('../../models').Meal
 
 module.exports = async function cleanup() {
   await Food.destroy({ where: {} })
+  await Meal.destroy({ where: {} })
 }
