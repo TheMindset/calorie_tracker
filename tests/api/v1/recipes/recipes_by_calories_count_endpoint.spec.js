b85b1115454335f0b44c9f1eccbba4c4615e2517
TheMindset <mulezig2014@outlook.fr>
2020-01-05 16:51:43 +0100

    Add spec for the total average calorie endpoint

---
+++ tests/api/v1/recipes/recipes_by_calories_count_endpoint.spec.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const request = require('supertest')
+
+const app = require('../../../../app')
+const cleanup = require('../../../helpers/test_clear_database')
+
+describe('average calories count api endpoint', () => {
+  beforeEach(async() =>{
+    await cleanup()
+  })
+
+  test('should return the total average colories by food type' , () => {
+    return request(app)
+    .get('/api/v1/total_average_calories?q=Banana')
+    .then(response => {
+      console.log(response.statusCode)
+      expect(response.statusCode).toBe(200)
+
+      expect(Object.keys(response.body.data.recipeSearch[0])).toContain('foodType')
+      expect(Object.keys(response.body.data.recipeSearch[0])).toContain('average')
+    })
+  })
+
+  test('should return 404 error when the food type is not found', () => {
+    return request(app)
+    .get('/api/v1/total_average_calories?q=pork')
+    .then(response => {
+      expect(response.statusCode).toBe(404)
+
+      expect(response.body.error).toBe('No food found for food type')
+    })
+  })
+
+  
+})
