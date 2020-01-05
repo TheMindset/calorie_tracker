9b6a7d289130bfc3db66c520458c94dc7804f6fc
TheMindset <mulezig2014@outlook.fr>
2020-01-05 19:28:03 +0100

    Add sorted ingredients routes

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -6,5 +6,6 @@ router.get('/search_foods', recipesController.searchFoods)
 router.get('/total_average_calories', recipesController.totalAverageCalorie)
 router.get('/num_of_ingredients', recipesController.numberOfIngredients)
 router.get('/sorted_prep_time', recipesController.sortedPrepTime)
+router.get('/sorted_ingredients', recipesController.sortedIngredients)
 
 module.exports = router
