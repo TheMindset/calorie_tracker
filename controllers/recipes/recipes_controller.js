f765bf8b495d6b047a88b80d63fc13bb52bf272c
TheMindset <mulezig2014@outlook.fr>
2020-01-05 17:33:11 +0100

    Add totalAverageColorie controller

--- controllers/recipes/recipes_controller.js
+++ controllers/recipes/recipes_controller.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch')
-const titleCase = require('../../helpers/title_case')
 
 const searchFoods = (request, response) => {
   return fetch(`https://calorie-tracker-self-ms.herokuapp.com/graphql?query={recipeSearch(foodType:"${request.query.q}"){id,name,foodType,recipeLink,totalCalories,numberOfIngredients,preparationTime}}`)
@@ -21,6 +20,27 @@ const searchFoods = (request, response) => {
   })
 }
 
+const totalAverageCalorie = (request, response) => {
+  return fetch(`https://calorie-tracker-self-ms.herokuapp.com/graphql?query={averageCalories(foodType:"${request.query.q}"){foodType, average}}`)
+  .then(recipeData => {
+    console.log(recipeData)
+    return recipeData.json()
+  })
+  .then(avarage => {
+    if (avarage.data.averageCalories.length === 0) {
+      response.setHeader('Content-Type', 'application/json')
+      response.status(404).send(JSON.stringify({ error: 'No food found for food type' }))
+    } else {
+      response.setHeader('Content-Type', 'application/json')
+      response.status(200).send(JSON.stringify(avarage))
+    }
+  })
+  .catch(error => {
+    response.setHeader('Content-Type', 'application/json')
+    response.status(500).send(JSON.stringify({ error }))
+  })
+}
+
 module.exports = {
-  searchFoods,
+  searchFoods, totalAverageCalorie
 }
