46b952ca10c15356d7f1c9672cf3d034638dc574
TheMindset <mulezig2014@outlook.fr>
2020-01-05 16:33:36 +0100

    Add recipes search foods routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const foods = require('./routes/api/v1/foods')
 const meals = require('./routes/api/v1/meals')
+const recipes = require('./routes/api/v1/recipes')
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/foods', foods);
 app.use('/api/v1/meals', meals)
+app.use('/api/v1/search_foods', recipes)
 
 module.exports = app;
